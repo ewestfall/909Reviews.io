7a04212bb253a643937d3fab5bac445e162f125d
Erin Westfall <ewestfall@appnexus.com>
2015-03-04 19:44:25 -0500

    created products columns and migrated it

--- reviews/db/migrate/20150304234806_create_products.rb
+++ reviews/db/migrate/20150304234806_create_products.rb
@@ -1,6 +1,9 @@
 class CreateProducts < ActiveRecord::Migration
   def change
     create_table :products do |t|
+    	t.string :name
+    	t.text :description
+    	t.string :image
 
       t.timestamps null: false
     end
