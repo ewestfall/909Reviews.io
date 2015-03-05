d0450c5edaa83c20a592427c56dd80f1bf0bcab1
Erin Westfall <ewestfall@appnexus.com>
2015-03-04 20:55:13 -0500

    added resourses to routes and got review view rendering in browser

--- reviews/app/controllers/reviews_controller.rb
+++ reviews/app/controllers/reviews_controller.rb
@@ -1,27 +1,28 @@
 class ReviewsController < ApplicationController
 
 	def index
-		@reviews = Review.all
+	# 	@reviews = Review.all
+		render "review"
 	end
 
-	def new
-		@reviews = Review.new
-	end
+	# def new
+	# 	@reviews = Review.new
+	# end
 
-	def create
-	end
+	# def create
+	# end
 
-	def edit
-	end
+	# def edit
+	# end
 
-	def update
-	end
+	# def update
+	# end
 
-	def delete
-	end
+	# def delete
+	# end
 
-	def destroy
-	end
+	# def destroy
+	# end
 
 
 end
--- reviews/config/routes.rb
+++ reviews/config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   
   root 'products#home'
   resources :reviews
+
+  get 'reviews' => 'reviews#review'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
